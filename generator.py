f31fdc329a3b991e0c61f57d8c6b66ffa8e652f8
natanmoura <natanmoura@escavador.com>
2023-05-31 11:13:57 -0300

    adição async e gerador corrigido

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@ import datetime
 
 from faker import Faker
 from bus import Bus
+from google.cloud.firestore import GeoPoint
 
 fake = Faker("pt_BR")
 Faker.seed(0)
@@ -9,9 +10,9 @@ Faker.seed(0)
 
 def generate_random_buses(n, route):
     buses_list = []
-    for _ in range(n):
+    for _ in range(n+1):
         last_location = fake.local_latlng(country_code="BR")
-        last_location = (float(last_location[0]), float(last_location[1]))
+        last_location = GeoPoint(float(last_location[0]), float(last_location[1]))
         bus = Bus(
             plate=fake.license_plate().replace("-", ""),
             last_updated=datetime.datetime.now(),
@@ -21,6 +22,3 @@ def generate_random_buses(n, route):
         if bus not in buses_list:
             buses_list.append(bus)
     return buses_list
-
-
-generate_random_buses(5, None)
--- main.py
+++ main.py
@@ -1,27 +1,40 @@
+import asyncio
 from pathlib import Path
 
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import firestore_async
 from dynaconf import Dynaconf
 from bus import Bus
+from generator import generate_random_buses
 
-settings = Dynaconf(
-    settings_files=[".settings.local.toml"],
-    root_path=Path(__file__).parent,
-    merge_enabled=True,
-)
 
-cred = credentials.Certificate(settings.CRED.json_name)
+async def main():
+    settings = Dynaconf(
+        settings_files=[".settings.local.toml"],
+        root_path=Path(__file__).parent,
+        merge_enabled=True,
+    )
 
-app = firebase_admin.initialize_app(cred)
+    cred = credentials.Certificate(settings.CRED.json_name)
 
-db = firestore.client()
+    firebase_admin.initialize_app(cred)
 
-buses = db.collection("buses")
-docs = buses.stream()
+    db = firestore_async.client()
 
-for doc in docs:
-    bus = Bus()
-    bus.build_from_doc(doc.to_dict())
-    print(str(bus))
+    buses = db.collection("buses")
+
+    b1_ref = db.collection("routes").document("b1")
+
+    buses_list = generate_random_buses(10000, route=b1_ref)
+
+    coros = []
+    for bus in buses_list:
+        coros.append(buses.add(bus.return_doc_format()))
+        if len(coros) == 50 or bus == buses_list[-1]:
+            await asyncio.gather(*coros)
+            coros = []
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
